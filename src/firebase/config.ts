8f51d116bb86fc1158215a18afd0b7bb32112f09
Francis Assuah <ynwbronxx360@gmail.com>
2026-01-02 02:18:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-818856677-f1c35",
+  "appId": "1:344877632024:web:f4c32c20103f15022f116d",
+  "apiKey": "AIzaSyCUHTEsG5TYzv2DHdMZ2uLcju2aUsazlBw",
+  "authDomain": "studio-818856677-f1c35.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "344877632024"
+};

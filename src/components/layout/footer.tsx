c9f56f1dc82afb4a28e1a6d12dac85a4cb40e4ce
Francis Assuah <ynwbronxx360@gmail.com>
2026-01-04 03:20:07 +0000

    this should be the current logo(X) not the old logo (_for element a_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -10,9 +10,29 @@ const navLinks = [
     { href: "/pilot-program", label: "Pilot" },
   ];
 
+function XIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        >
+        <path d="M18 6 6 18" />
+        <path d="m6 6 12 12" />
+        </svg>
+    )
+}
+
 const socialLinks = [
     { name: "Facebook", icon: Facebook, href: "#" },
-    { name: "Twitter", icon: Twitter, href: "#" },
+    { name: "X", icon: XIcon, href: "#" },
     { name: "Instagram", icon: Instagram, href: "#" },
     { name: "LinkedIn", icon: Linkedin, href: "#" },
 ]
